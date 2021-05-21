0b7f81412d5dc99cdefe83b6c5b9ec99a0c997b4
Pooja <pooja.14206@gmail.com>
2021-05-21 11:50:13 +0530

    added a file to add junit

--- src/test/java/com/bridgelabz/AddressbookServiceTest.java
+++ src/test/java/com/bridgelabz/AddressbookServiceTest.java
@@ -1,18 +1,18 @@
-//package com.bridgelabz;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//import java.sql.SQLException;
-//import java.util.List;
-//
-//public class AddressbookServiceTest {
-//    @Test
-//    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() throws AddressbookException, SQLException {
-//        AddressbookServiceImplementation addressbookServiceImplementation = new AddressbookServiceImplementation();
-//        List<AddressbookData> addressbookData = addressbookServiceImplementation.getAddressbook();
-//        System.out.println(addressbookData);
-//        Assert.assertEquals(9, addressbookData.size());
-//    }
+package com.bridgelabz;
 
-//}
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class AddressbookServiceTest {
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() throws AddressbookException, SQLException {
+        AddressbookServiceImplementation addressbookServiceImplementation = new AddressbookServiceImplementation();
+        List<AddressbookData> addressbookData = addressbookServiceImplementation.getAddressbook();
+        System.out.println(addressbookData);
+        Assert.assertEquals(9, addressbookData.size());
+    }
+
+}
